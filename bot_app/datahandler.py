0249b012a0cfe660fb729944125f5b59da505e2e
mikhailvilkin <vilkinmikhail@yandex.ru>
2023-01-05 21:01:40 +0300

    currency names fix

--- bot_app/datahandler.py
+++ bot_app/datahandler.py
@@ -12,8 +12,8 @@ def _get_crypto_currencies():
         p = el.text.split(' ')
         if (len(p) > 2):
             r[p[1]] = {
-                'full_name': p[2], 
-                'in_usd': p[5],
+                'full_name': p[2] if p[3] == '' else p[2] + ' ' + p[3], 
+                'in_usd': p[6] if p[5] == '$' else p[5],
             }
     return pretty_print_dict(r)
 
@@ -23,7 +23,7 @@ def pretty_print_dict(d: dict) -> str:
         result = result + "<b>" + key + "</b> (" + value["full_name"] + "): $" + value["in_usd"] + "\n"
     return result
 
-# regexp = re.compile(r"<a onclick=\"update_hash\(&#\d+;.+&#\d+;\);\" href=\"javascript:void\(0\);\">(.*)</a>")
+# # regexp = re.compile(r"<a onclick=\"update_hash\(&#\d+;.+&#\d+;\);\" href=\"javascript:void\(0\);\">(.*)</a>")
 # link = "https://bitinfocharts.com/cryptocurrency-prices/#CNY"
 
 # data = requests.get(link)
@@ -34,15 +34,11 @@ def pretty_print_dict(d: dict) -> str:
 # # for el in res[1:]:
 # #     print(el.text)
 
-# # regexp = r"(\w+)\s+(\w+)\s+\$\s+([\d,\.]+)\s+(.+)\s+in"
-# # pattern = re.compile(regexp)
-# r = {}
-# for el in res[1:]:
-#     p = el.text.split(' ')
-#     if (len(p) > 2):
-#         r[p[1]] = {
-#             'full_name': p[2], 
-#             'in_usd': p[5],
-#         }
+# # # regexp = r"(\w+)\s+(\w+)\s+\$\s+([\d,\.]+)\s+(.+)\s+in"
+# # # pattern = re.compile(regexp)
+# # r = {}
+# for el in res[1:10]:
+#     # p = el.text.split(' ')
+#     print(el.text)
 
-# print(r)
+# # print(r)
